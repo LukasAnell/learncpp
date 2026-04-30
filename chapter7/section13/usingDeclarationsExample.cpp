565ef40885306fe423a6499607c18f5f2b3eb114
LukasAnell <lukasmanell@gmail.com>
2026-04-29 22:34:28 -0400

    Example showing one way to use the using keyword without encompassing too much

---
+++ chapter7/section13/usingDeclarationsExample.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+   using std::cout; // this using declaration tells the compiler that cout should resolve to std::cout
+   cout << "Hello world!\n"; // so no std:: prefix is needed here!
+
+   return 0;
+} // the using declaration expires at the end of the current scope
